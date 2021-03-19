03837434fd04105dcd88347578598fae1b39b88d
Jazaltron <jazaltron10@gmail.com>
2021-03-19 23:47:02 +0100

    little changes

--- server.js
+++ server.js
@@ -4,11 +4,47 @@
 
 
 
+//WRITING TO FILES
+const {writeFile, writeFileSync, write} = require('fs');
+
+const newContent = "This is some new text"
+
+writeFile("text.txt", newContent, (err)=>{
+    if (err){
+        console.error(err);
+        return;
+    }
+    console.log("Content Written!");
+});
 
 
-const fs = require('fs');
 
 
+//READING FILES ASYNCHRONOUSLY
+/* const fs = require('fs');
+fs.readFile('text.txt', "utf8", (err, data)=>{
+    if(err){
+        console.error(err);
+        return;
+    }
+    console.log(data); // OR console.log(data.toString());
+});
+
+console.log("Log from Outside even though this code comes last => Asynchronicity at work"); */
+
+
+
+
+
+//SYNCHRONOUS VERSION
+/* try {
+    const data = fs.readFileSync("text.txt","utf8");
+    console.log(data);
+}catch(err){
+    console.error(err);
+}
+console.log("Log from Outside even though this code comes last => Asynchronicity at work");
+ */
 
 
 
